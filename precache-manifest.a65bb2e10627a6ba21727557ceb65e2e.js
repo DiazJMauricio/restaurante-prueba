05faf90a4493e417de4c556f449fc06800ee0067
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2019-02-26 00:47:44 -0300

    Updates

---
+++ precache-manifest.a65bb2e10627a6ba21727557ceb65e2e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1379aeab89146fbe1e036669e1e9c04e",
+    "url": "/restaurante-prueba/static/media/bg1_small.1379aeab.jpg"
+  },
+  {
+    "revision": "ad58993a8395368e8f93",
+    "url": "/restaurante-prueba/static/css/main.ebb7223f.chunk.css"
+  },
+  {
+    "revision": "91bc4b32a8db7fea43d9",
+    "url": "/restaurante-prueba/static/js/runtime~main.91bc4b32.js"
+  },
+  {
+    "revision": "9c8f587d67de48892b47",
+    "url": "/restaurante-prueba/static/js/2.9c8f587d.chunk.js"
+  },
+  {
+    "revision": "3122cc4b9648d9c72613b81e3fcf70ba",
+    "url": "/restaurante-prueba/static/media/bg1_Large.3122cc4b.jpg"
+  },
+  {
+    "revision": "c2600c1e8315994b7ebc8e8b614b166b",
+    "url": "/restaurante-prueba/static/media/bg1_extraSmall.c2600c1e.jpg"
+  },
+  {
+    "revision": "ad58993a8395368e8f93",
+    "url": "/restaurante-prueba/static/js/main.ad58993a.chunk.js"
+  },
+  {
+    "revision": "7cbfc4f910c443d603fb033f1331d28d",
+    "url": "/restaurante-prueba/static/media/bg1_Medium.7cbfc4f9.jpg"
+  },
+  {
+    "revision": "b8f931218522f5dbd08d4112f4949a75",
+    "url": "/restaurante-prueba/static/media/pizzaSmall.b8f93121.jpg"
+  },
+  {
+    "revision": "0291668f808ce8024b40e651a9fa7a00",
+    "url": "/restaurante-prueba/static/media/pizzaMedium.0291668f.jpg"
+  },
+  {
+    "revision": "3ed0dbe3f2126abaab6a8bbc4d5ed918",
+    "url": "/restaurante-prueba/static/media/pizzaLarge.3ed0dbe3.jpg"
+  },
+  {
+    "revision": "29ec5fa6f17d4174502a4dcee726f35d",
+    "url": "/restaurante-prueba/static/media/burger.29ec5fa6.jpg"
+  },
+  {
+    "revision": "f612fafd3cb484fb523a2ab19349b0f0",
+    "url": "/restaurante-prueba/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/restaurante-prueba/precache-manifest.a65bb2e10627a6ba21727557ceb65e2e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/restaurante-prueba/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
